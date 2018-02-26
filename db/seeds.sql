7bdfb02c819fe2774946e814997e878e45bfd96c
Marcelo Del Solar <solarg7@Marcelos-MacBook-Pro.local>
2018-02-26 18:35:01 -0500

    HW12

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Caribean Cheese Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Royal Egg Cheese Burger', true);
